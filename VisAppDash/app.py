9daeec4d0c63faafe7d5e873c243ce6ab399f5f8
abombin <abombin93@gmail.com>
2022-08-21 20:36:37 -0400

    added dash app

---
+++ VisAppDash/app.py
@@ -0,0 +1,23 @@
+from dash import Dash, dcc, html, Input, Output, callback
+from pages import page1, page2
+
+
+app = Dash(__name__, suppress_callback_exceptions=True)
+server = app.server
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+@callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/page2':
+        return page2.layout
+    else:
+        return page1.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
